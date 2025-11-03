3bbe1a18f807ab2008116ba8ca5a199f747f7438
ryo112345 <ryo112345swe@gmail.com>
2025-11-03 12:02:37 +0900

    userモデル作成

---
+++ api-server/internal/models/user.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Email     string         `gorm:"size:255;uniqueIndex;not null" json:"email"`
+	Name      string         `gorm:"size:100;not null" json:"name"`
+	Password  string         `gorm:"size:255;not null" json:"-"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
