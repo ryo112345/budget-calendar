74521b844710c1d65e778c9ef8a290a5ca04790f
ryo112345 <ryo112345swe@gmail.com>
2025-11-03 10:40:53 +0900

    Initial commit

---
+++ api-server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// ヘルスチェック用のエンドポイント
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"status": "ok",
+		})
+	})
+
+	// サーバー起動
+	e.Logger.Fatal(e.Start(":8080"))
+}
