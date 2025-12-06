aa067c724f91a7df09590de05d48413ab70d65a9
ryo112345 <ryo112345swe@gmail.com>
2025-12-06 13:52:02 +0900

    add

--- api-server/cmd/server/main.go
+++ api-server/cmd/server/main.go
@@ -1,34 +1,57 @@
 package main
 
 import (
+	api "apps/apis"
+	"apps/database"
+	"apps/internal/handlers"
+	"apps/internal/middlewares"
+	"apps/internal/services"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-	api "github.com/yamao/budget-calendar/apis"
-	"github.com/yamao/budget-calendar/database"
-	"github.com/yamao/budget-calendar/internal/handlers"
-	"github.com/yamao/budget-calendar/internal/services"
 )
 
 func main() {
-	godotenv.Load()
+	// NOTE: デプロイ先の環境はSecret Managerで環境変数を管理する
+	if os.Getenv("APP_ENV") != "production" {
+		loadEnv()
+	}
 
-	database.Init()
+	dbCon := database.Init()
 
+	// NOTE: service層のインスタンス
+	userService := services.NewUserService(dbCon)
+	categoryService := services.NewCategoryService(dbCon)
+
+	// NOTE: Handlerのインスタンス
+	csrfHandler := handlers.NewCsrfHandler()
+	usersHandler := handlers.NewUsersHandler(userService)
+	categoriesHandler := handlers.NewCategoriesHandler(categoryService)
+
+	// NOTE: Echoインスタンスとミドルウェアの設定
 	e := echo.New()
+	middlewares.Register(e)
 
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{
-			"status": "ok",
-		})
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Budget Calendar!")
 	})
 
-	categoryService := services.NewCategoryService(database.DB)
-	categoryHandler := handlers.NewCategoriesHandler(categoryService)
+	// NOTE: Handlerをルーティングに追加
+	mainHandler := handlers.NewMainHandler(csrfHandler, usersHandler, categoriesHandler)
+	mainStrictHandler := api.NewStrictHandler(mainHandler, []api.StrictMiddlewareFunc{middlewares.AuthMiddleware})
+	api.RegisterHandlers(e, mainStrictHandler)
 
-	// OpenAPI仕様に基づいたルーティングを登録
-	api.RegisterHandlers(e, categoryHandler)
+	if err := e.Start(":8080"); err != nil && err != http.ErrServerClosed {
+		e.Logger.Errorf("Echo server error: %v", err)
+	}
+}
 
-	e.Logger.Fatal(e.Start(":8080"))
+func loadEnv() {
+	envFilePath := os.Getenv("ENV_FILE_PATH")
+	if envFilePath == "" {
+		envFilePath = ".env"
+	}
+	godotenv.Load(envFilePath)
 }
